b736f3fff46da33f81709afaa23179fc763af97b
adelbertclerge <adelbertclerge@gmail.com>
2021-01-06 20:30:51 -0500

    bil_cli

--- lib/bil_cli/cli.rb
+++ lib/bil_cli/cli.rb
@@ -1,8 +1,7 @@
 class CLI
-
     
     def start
-        puts "Welcome To Ball Is Life, Where you can learn about your favorite NBA teams."
+        puts "Welcome To Ball Is Life, where you can learn about your favorite NBA teams."
         puts "Let's start with your name:"
         API.get_data
         greet(user_input)
@@ -16,36 +15,59 @@ class CLI
         puts "Great #{name}, which team would you like to learn about?"
         puts "Input 'y' to see the list, exit' to leave"
         menu
-end
-
-def teams_list
-    Teams.all.each.with_index(1) do |team, i| 
-        puts "#{i}. #{team.name}"
     end
 
-    team_name
-end
+    def teams_list
+        Teams.all.each.with_index(1) do |team, i| 
+        puts "#{i}. #{team.name}"
+        end
+        team_name
+    end
 
-def goodbye
-    puts "Hope you enjoyed learning about your favorite teams! Have a great Day! "
-end
+    def goodbye
+        puts "Hope you enjoyed learning about your favorite teams! Have a great Day! "
+    end
 
-def invalid
-    puts "Hmm, that doesnt seem valid, try again?"
-    menu
-end
+    def invalid
+        puts "Hmm, that doesnt seem valid, try again?"
+        menu
+    end
 
-def team_selection
-    puts "select a team for more details"
+    def team_selection
+        puts "select a team for more details"
 
-    selection = user_input
-    puts "#{selection}"
-   team = Teams.find_team(selection) 
-   team_details(team)
-end
+        selection = user_input
+        puts "#{selection}"
+        team = Teams.find_team(selection) 
+        team_details(team)
+    end
 
-def team_name
-    name = Teams.all.each{|team| team.name}
+    def team_name
+        name = Teams.all.each{|team| team.name}
+    end
+
+    def menu 
+        selection = user_input
+        # puts "Please be patient. "
+
+        if selection == 'y'
+            teams_list
+        elsif selection == 'exit'
+            goodbye
+        else
+            invalid
+        end
+        team_selected
+    end
+
+    def team_selected
+        selection = user_input
+        selection = selection.to_i-1
+        selected_team = Teams.all[selection]
+        binding.pry
+        #selection.to_i-1
+    end
+
 end
 
 
@@ -55,19 +77,3 @@ end
 #     puts "City: #{team.city}"
 #     menu
 # end
-
-def menu 
-    selection = user_input
-   # puts "Please be patient. "
-
-    if selection == 'y'
-        teams_list
-        menu
-    elsif selection == 'exit'
-        goodbye
-    else
-         invalid
-    end
-end
-
-end
